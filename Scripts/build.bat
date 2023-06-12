ffe7a4d654b2a7105fd31e9939769034d03975bd
tergav17 <gctersteeg@gmail.com>
2023-06-12 00:09:19 -0500

    Started work on text fix utility

--- Scripts/build.bat
+++ Scripts/build.bat
@@ -63,6 +63,11 @@ move init.lst ..\Output\Application\Nabu\Listings >NUL
 ..\Build\zasm futil.asm -u -w -b futil.com
 move futil.com ..\Directory >NUL
 move futil.lst ..\Output\Application\Nabu\Listings >NUL
+cd Misc\TxtFix
+..\..\..\Build\zasm txtfix.asm -u -w -b txtfix.com
+move txtfix.com ..\..\..\Directory >NUL
+move txtfix.lst ..\..\..\Output\Application\Nabu\Listings >NUL
+cd ..\..
 
 REM Assemble the disk image
 cd ..\Build\cpmtools
