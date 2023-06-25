228f208249724dbdd5615e06e375a62233f6f2c8
tergav17 <gctersteeg@gmail.com>
2023-06-24 19:02:01 -0500

    More work in breaking out common HCCA routines from NDSK and NFS

--- System/dev/nabu_hcca.asm
+++ System/dev/nabu_hcca.asm
@@ -17,93 +17,95 @@
 ; exposed
 ;
 ; uses: a, b
-ns_hini:ld	a,0x07
-	out	(ns_atla),a	; AY register = 7
-	in	a,(ns_ayda)
+hc_hini:ld	a,0x07
+	out	(hc_atla),a	; AY register = 7
+	in	a,(hc_ayda)
 	and	0x3F
 	or	0x40
-	out	(ns_ayda),a	; Configure AY port I/O
+	out	(hc_ayda),a	; Configure AY port I/O
 	
 	; Claim interrupt vectors
 	push	hl
-	ld	hl,ns_rirq
+	ld	hl,hc_rirq
 	ld	(intvec),hl
-	ld	hl,ns_wirq
+	ld	hl,hc_wirq
 	ld	(intvec+2),hl
 	pop	hl
 	
 ; Set interrupts to their default state
 ;
 ; uses: a
-ns_dflt:ld	a,0x0E
-	out	(ns_atla),a	; AY register = 14
+hc_dflt:ld	a,0x0E
+	out	(hc_atla),a	; AY register = 14
 	ld	a,0xB0
-	out	(ns_ayda),a	; Enable HCCA receive and but not send, plus key and VDP
+	out	(hc_ayda),a	; Enable HCCA receive and but not send, plus key and VDP
 	
-ns_dfl0:ld	a,0x0F		
-	out	(ns_atla),a	; AY register = 15
+hc_dfl0:ld	a,0x0F		
+	out	(hc_atla),a	; AY register = 15
 	
 	ret
 
 ; Set receive and send interrupts
 ;
 ; uses: a
-ns_esnd:ld	a,0x0E
-	out	(ns_atla),a	; AY register = 14
+hc_esnd:ld	a,0x0E
+	out	(hc_atla),a	; AY register = 14
 	ld	a,0xC0
-	out	(ns_ayda),a	; Enable HCCA receive and send
-	jr	ns_dfl0
+	out	(hc_ayda),a	; Enable HCCA receive and send
+	jr	hc_dfl0
 	
 ; Set receive but not send interrupt
 ;
 ; uses: a
-ns_dsnd:ld	a,0x0E
-	out	(ns_atla),a	; AY register = 14
+hc_dsnd:ld	a,0x0E
+	out	(hc_atla),a	; AY register = 14
 	ld	a,0x80
-	out	(ns_ayda),a	; Enable HCCA receive and but not send
-	jr	ns_dfl0
+	out	(hc_ayda),a	; Enable HCCA receive and but not send
+	jr	hc_dfl0
 
 ; Gets a block from the currently open file
 ; and places it in (hl)
+; a = Target file descriptor
 ; de = Block to read
 ; hl = Destination for information
 ;
 ; Returns location directly after in hl
 ; Carry flag set on error
 ; uses: af, b, hl
-ns_getb:call	ns_get0
-	jp	ns_dflt
-ns_get0:ex	de,hl
-	ld	(ns_m2bn),hl
+hc_getb:ld	(hc_m2fd),a
+	call	hc_get0
+	jp	hc_dflt
+hc_get0:ex	de,hl
+	ld	(hc_m2bn),hl
 	ex	de,hl
 	push	hl
-	ld	hl,ns_m2
+	ld	hl,hc_m2
 	ld	b,12
-	call	ns_send
+	call	hc_send
 	pop	hl
 	ret	c
-	call	ns_hcrd
-	call	ns_hcre
+	call	hc_hcrd
+	call	hc_hcre
 	ret	c
 	cp	0x84
 	scf
-	jr	nz,ns_get2
-	call	ns_hcre
-	ld	(ns_tran),a
+	jr	nz,hc_get2
+	call	hc_hcre
+	ld	(hc_tran),a
 	ld	b,a
-	call	ns_hcre
+	call	hc_hcre
 	ld	a,b
 	or	a
 	ret	z
-ns_get1:call	ns_hcre
+hc_get1:call	hc_hcre
 	ret	c
 	ld	(hl),a
 	inc	hl
-	djnz	ns_get1
+	djnz	hc_get1
 	or	a
 	ret
-ns_get2:call	ns_hcrd	; Read the error message and exit
-	call	ns_hcre
+hc_get2:call	hc_hcrd	; Read the error message and exit
+	call	hc_hcre
 	scf
 	ret
 	
@@ -114,26 +116,27 @@ ns_get2:call	ns_hcrd	; Read the error message and exit
 ;
 ; Carry flag set on error
 ; uses: af, b, hl
-ns_putb:call	ns_put0
-	jp	ns_dflt
-ns_put0:ex	de,hl
-	ld	(ns_m3bn),hl
+hc_putb:ld	(hc_m3fd),a
+	call	hc_put0
+	jp	hc_dflt
+hc_put0:ex	de,hl
+	ld	(hc_m3bn),hl
 	ex	de,hl
 	push	hl
-	ld	hl,ns_m3
+	ld	hl,hc_m3
 	ld	b,12
-	call	ns_send		; Send message precursor
+	call	hc_send		; Send message precursor
 	pop	hl
 	ret	c
 	ld	b,128
-ns_put1:ld	a,(hl)		; Send the block
-	call	ns_hcwr
+hc_put1:ld	a,(hl)		; Send the block
+	call	hc_hcwr
 	ret	c
 	inc	hl
-	djnz	ns_put1
-	ld	hl,ns_buff
-	call	ns_rece
-	ld	a,(ns_buff)
+	djnz	hc_put1
+	ld	hl,hc_buff
+	call	hc_rece
+	ld	a,(hc_buff)
 	cp	0x81
 	ret	z
 	scf
@@ -144,21 +147,21 @@ ns_put1:ld	a,(hl)		; Send the block
 ;
 ; Carry flag set on error
 ; uses: af, b, hl
-ns_rece:call	ns_dsnd
-	call	ns_rec0
-	jp	ns_dflt
-ns_rec0:call	ns_hcre
+hc_rece:call	hc_dsnd
+	call	hc_rec0
+	jp	hc_dflt
+hc_rec0:call	hc_hcre
 	ret	c		; Existing error
 	ld	b,a
-	call	ns_hcre
+	call	hc_hcre
 	ret	c		; Existing error
 	scf
 	ret	nz		; Message too big!
-ns_rec1:call	ns_hcre
+hc_rec1:call	hc_hcre
 	ret	c		; Error!
 	ld	(hl),a
 	inc	hl
-	djnz	ns_rec1
+	djnz	hc_rec1
 	or	a
 	ret
 	
@@ -168,11 +171,11 @@ ns_rec1:call	ns_hcre
 ;
 ; Carry flag set on error
 ; uses: af, b, hl
-ns_send:ld	a,(hl)
+hc_send:ld	a,(hl)
 	inc	hl
-	call	ns_hcwr
+	call	hc_hcwr
 	ret	c		; Error!
-	djnz	ns_send
+	djnz	hc_send
 	ret
 	
 ; Read from the HCCA port
@@ -182,44 +185,44 @@ ns_send:ld	a,(hl)
 ; Returns result in a
 ; Carry flag set on error
 ; Uses: af
-ns_hcrd:call	ns_hcre
-ns_hcre:xor	a
-	ld	(ns_inf),a
+hc_hcrd:call	hc_hcre
+hc_hcre:xor	a
+	ld	(hc_inf),a
 	push	de
 	ld	a,0x09
-	out	(ns_nctl),a	; Turn on recv light
+	out	(hc_nctl),a	; Turn on recv light
 	ld	de,0xFFFF
-ns_hcr0:ld	a,(ns_inf)
+hc_hcr0:ld	a,(hc_inf)
 	or	a
-	jr	nz,ns_hcr2
-	in	a,(ns_ayda)
+	jr	nz,hc_hcr2
+	in	a,(hc_ayda)
 	;bit	0,a
-	;jr	z,ns_hcr0	; Await an interrupt
+	;jr	z,hc_hcr0	; Await an interrupt
 	;bit	1,a
-	;jr	z,ns_hcr1
+	;jr	z,hc_hcr1
 	and	0x0F
 	xor	0b00000001
-	jr	z,ns_hcr1
+	jr	z,hc_hcr1
 	dec	de
 	ld	a,e
 	or	d
-	jr	nz,ns_hcr0
-ns_hcer:ld	a,0x01
-	out	(ns_nctl),a	; Turn off recv light
+	jr	nz,hc_hcr0
+hc_hcer:ld	a,0x01
+	out	(hc_nctl),a	; Turn off recv light
 	scf
 	pop	de
 	ret			; Timed out waiting
-ns_hcr1:ld	a,0x01
-	out	(ns_nctl),a	; Turn off recv light
-	in	a,(ns_hcca)
+hc_hcr1:ld	a,0x01
+	out	(hc_nctl),a	; Turn off recv light
+	in	a,(hc_hcca)
 	pop	de
 	or	a
 	ret
-ns_hcr2:ld	a,0x01
-	out	(ns_nctl),a	; Turn off recv light
+hc_hcr2:ld	a,0x01
+	out	(hc_nctl),a	; Turn off recv light
 	xor	a
-	ld	(ns_inf),a
-	ld	a,(ns_inb)
+	ld	(hc_inf),a
+	ld	a,(hc_inb)
 	pop	de
 	ret
 	
@@ -227,11 +230,11 @@ ns_hcr2:ld	a,0x01
 ; Reads from the HCCA, buffers it, and then sets the flag
 ;
 ; uses: none
-ns_rirq:push	af
-	in	a,(ns_hcca)
-	ld	(ns_inb),a
+hc_rirq:push	af
+	in	a,(hc_hcca)
+	ld	(hc_inb),a
 	ld	a,1
-	ld	(ns_inf),a
+	ld	(hc_inf),a
 	pop	af
 	ei
 	ret
@@ -244,48 +247,66 @@ ns_rirq:push	af
 ;
 ; Carry flag set on error
 ; Uses: f
-ns_hcwr:push	de
-	ld	(ns_outb),a
+hc_hcwr:push	de
+	ld	(hc_outb),a
 	xor	a
-	ld	(ns_outf),a
-	call	ns_esnd
+	ld	(hc_outf),a
+	call	hc_esnd
 	ld	de,0xFFFF
 	ld	a,0x21
-	out	(ns_nctl),a	; Turn on send light
-ns_hcw0:ld	a,(ns_outf)
+	out	(hc_nctl),a	; Turn on send light
+hc_hcw0:ld	a,(hc_outf)
 	or	a
-	jr	nz,ns_hcw2
-	in	a,(ns_ayda)
+	jr	nz,hc_hcw2
+	in	a,(hc_ayda)
 	;bit	0,a
-	;jr	z,ns_hcw0	; Await an interrupt
+	;jr	z,hc_hcw0	; Await an interrupt
 	;bit	1,a
-	;jr	nz,ns_hcw1
+	;jr	nz,hc_hcw1
 	and	0x0F
 	xor	0b00000011
-	jr	z,ns_hcw1
+	jr	z,hc_hcw1
 	dec	de
 	ld	a,e
 	or	d
-	jr	nz,ns_hcw0
-	call	ns_dsnd
-	jr	ns_hcer		; Timed out waiting
-ns_hcw1:ld	a,(ns_outb)
-	out	(ns_hcca),a
-ns_hcw2:pop	de
+	jr	nz,hc_hcw0
+	call	hc_dsnd
+	jr	hc_hcer		; Timed out waiting
+hc_hcw1:ld	a,(hc_outb)
+	out	(hc_hcca),a
+hc_hcw2:pop	de
 	ld	a,0x01
-	out	(ns_nctl),a	; Turn off send light
-	call	ns_dsnd
+	out	(hc_nctl),a	; Turn off send light
+	call	hc_dsnd
 	or	a
 	ret
 	
 ; HCCA write interrupt
 ; Writes to the HCCA from the buffer, and 
-ns_wirq:push	af
-	ld	a,(ns_outb)
-	out	(ns_hcca),a
+hc_wirq:push	af
+	ld	a,(hc_outb)
+	out	(hc_hcca),a
 	ld	a,1
-	ld	(ns_outf),a
-	call	ns_dsnd		; Y'all can't behave, turning off
+	ld	(hc_outf),a
+	call	hc_dsnd		; Y'all can't behave, turning off
 	pop	af
 	ei
 	ret
+	
+; Message prototype to read a block
+; Total length: 12 bytes
+hc_m2:	defb	0x8F,0x00
+	defw	8		; Message length
+	defb	0x07		; Cmd: STORAGE-GET-BLOCK
+hc_m2fd:defb	hc_fild		; Default file descritor
+hc_m2bn:defw	0x00,0x00	; Block number
+	defw	128		; Block length
+	
+; Message prototype to write a block
+; Total length: 12 bytes
+hc_m3:	defb	0x8F,0x00
+	defw	136		; Message length
+	defb	0x08		; Cmd: STORAGE-PUT-BLOCK
+hc_m3fd:defb	hc_fild		; Default file descritor
+hc_m3bn:defw	0x00,0x00	; Block number
+	defw	128		; Block length
